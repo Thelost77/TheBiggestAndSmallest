784173b1fac11c4483e304bedb7a69bf5a7bb29f
Thelost77 <wikzie17@gmail.com>
2021-06-21 09:24:46 +0200

    Complete program

---
+++ TheBiggestAndSmallest/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+
+        private static string TheBiggestAndSmallest(string givenNumbers)
+        {
+            var listOfGivenNumbers = new List<int>();
+            string addComponent = null;
+            int i = 0;
+            foreach (char c in givenNumbers)
+            {
+                if (c != ' ' && givenNumbers.Contains(' '))
+                {
+                    addComponent = addComponent + c.ToString();
+                    i++;
+                }
+
+                if (c == ' ' || i == givenNumbers.Length  && givenNumbers.Contains(' '))
+                {
+                    listOfGivenNumbers.Add(int.Parse(addComponent));
+                    addComponent = null;
+                    i++;
+                }
+                if (!givenNumbers.Contains(' ') && givenNumbers.Length == 1)
+                {
+                    listOfGivenNumbers.Add(int.Parse(c.ToString()));
+                    break;
+                }
+
+                if(!givenNumbers.Contains(' ') && givenNumbers.Length > 1)
+                {
+                    addComponent = addComponent + c.ToString();
+                    i++;
+                    if (i == givenNumbers.Length)
+                    {
+                        listOfGivenNumbers.Add(int.Parse(addComponent));
+                        break;
+                    }
+                }
+            }
+            string outcome = listOfGivenNumbers.Max().ToString();
+            outcome = outcome + " " + listOfGivenNumbers.Min().ToString();
+            return outcome;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj, podaj mi listę liczb całkowitych oddzielonych od siebie znakiem space.");
+            var numbers = Console.ReadLine();
+            Console.WriteLine(TheBiggestAndSmallest(numbers));
+            
+        }
+    }
+}
